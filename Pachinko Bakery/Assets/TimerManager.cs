c32a4402ba3866641c09ccedc039ea30c3c67fc5
Benjamin Barbeau <73616169+Ategon@users.noreply.github.com>
2021-11-26 15:05:01 -0500

    Add level timer

---
+++ Pachinko Bakery/Assets/TimerManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerManager : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameplayManager gameplayManager;
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private float minuteLength;
+
+    void FixedUpdate()
+    {
+        int hour = 8;
+        int mins = 0;
+        bool pm = false;
+
+        mins = (int) Mathf.Floor(gameplayManager.LevelTimer / minuteLength) % 6;
+        hour += (int) (Mathf.Floor(gameplayManager.LevelTimer / minuteLength) - mins) / 6;
+
+        mins *= 10;
+        string minString = mins.ToString("00"); //needed for 00 edge case
+
+        if (hour > 12)
+        {
+            hour -= 12;
+            pm = true;
+        }
+
+        timerText.text = $"{hour}:{minString} {(pm ? "pm" : "am")}";
+    }
+}
