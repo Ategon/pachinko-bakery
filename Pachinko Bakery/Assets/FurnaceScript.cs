3e3c7f2a72da84d5dd6a275e07cd8046d55371b3
Benjamin Barbeau <73616169+Ategon@users.noreply.github.com>
2021-11-27 15:57:58 -0500

    Add furnace heat dial

--- Pachinko Bakery/Assets/FurnaceScript.cs
+++ Pachinko Bakery/Assets/FurnaceScript.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FurnaceScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private TextMeshProUGUI heatText;
+    [SerializeField] private int heatState = 1;
+    [SerializeField] private string[] heatTexts;
+    [SerializeField] private Color[] heatColors;
+
     void Start()
     {
-        
+        heatText.text = heatTexts[heatState];
+        heatText.color = heatColors[heatState];
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeHeat()
     {
-        
+        heatState++;
+        if (heatState >= 4) heatState = 0;
+        heatText.text = heatTexts[heatState];
+        heatText.color = heatColors[heatState];
     }
 }
